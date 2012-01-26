3d3276cdc0553731d3782b03f0bec03628eff041
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-01-26 23:59:01 +0100

    updated kernel.

--- sylius/SyliusKernel.php
+++ sylius/SyliusKernel.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Sylius sandbox application.
+ * This file is part of the Sylius package.
  *
  * (c) Paweł Jędrzejewski
  *
@@ -11,70 +11,80 @@
 
 namespace Sylius;
 
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\ClassLoader\DebugUniversalClassLoader;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+use Symfony\Component\HttpKernel\Kernel;
+
 
 /**
  * Sylius kernel.
  * Powered by Symfony2.
- * 
+ *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
 class SyliusKernel extends Kernel
-{   
+{
     /**
      * Register bundles in kernel.
      */
     public function registerBundles()
     {
-        if ('installer' == $this->getEnvironment()) {
-            $bundles = array(
-                new \Sylius\Bundle\CoreBundle\SyliusCoreBundle(),
-                new \Sylius\Bundle\InstallerBundle\SyliusInstallerBundle(),
-                
-                /*
-                 * Third party bundles.
-                 */
-                new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-                new \Symfony\Bundle\TwigBundle\TwigBundle(),
-                new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-                new \Symfony\Bundle\MonologBundle\MonologBundle(),
-                new \Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-            );
-        } else {
-            $bundles = array(
-                /*
-                 * Third party bundles.
-                 */
-                new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-                new \Symfony\Bundle\TwigBundle\TwigBundle(),
-                new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-                new \Symfony\Bundle\MonologBundle\MonologBundle(),
-                new \Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-                new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-                new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-                new \Liip\ThemeBundle\LiipThemeBundle(),
-                       
-                /*
-                 * Sylius bundles.
-                 */
-                new \Sylius\Bundle\CoreBundle\SyliusCoreBundle(),
-                new \Sylius\Bundle\CatalogBundle\SyliusCatalogBundle(),
-                new \Sylius\Bundle\AssortmentBundle\SyliusAssortmentBundle(),
-                new \Sylius\Bundle\CartBundle\SyliusCartBundle(),
-                new \Sylius\Bundle\ThemingBundle\SyliusThemingBundle(),
-                new \Sylius\Bundle\SalesBundle\SyliusSalesBundle(),
-                new \Sylius\Bundle\InstallerBundle\SyliusInstallerBundle(),
-            );
-            
+        $bundles = array(
+
+            /*
+             * Third party bundles.
+             */
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+
+            /*
+             * Sylius bundles.
+             */
+            new \Sylius\Bundle\CoreBundle\SyliusCoreBundle(),
+            new \Sylius\Bundle\CatalogBundle\SyliusCatalogBundle(),
+            new \Sylius\Bundle\AssortmentBundle\SyliusAssortmentBundle(),
+            new \Sylius\Bundle\CartBundle\SyliusCartBundle(),
+            new \Sylius\Bundle\ThemingBundle\SyliusThemingBundle(),
+            new \Sylius\Bundle\SalesBundle\SyliusSalesBundle(),
+            new \Sylius\Bundle\InstallerBundle\SyliusInstallerBundle(),
+            new \Sylius\Bundle\CheckoutsBundle\SyliusCheckoutsBundle(),
+            new \Sylius\Bundle\PricingBundle\SyliusPricingBundle()
+
+        );
+
         $bundles[] = new \Sylius\Bundle\PluginsBundle\SyliusPluginsBundle($this, $bundles);
-        }
-        
+
         if ($this->isDebug()) {
             $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
         }
-        
+
         return $bundles;
+    }
+
+    /**
+     * Init kernel.
+     */
+    public function init()
+    {
+        if ($this->isDebug()) {
+            ini_set('display_errors', 1);
+            error_reporting(-1);
+
+            DebugUniversalClassLoader::enable();
+            ErrorHandler::register();
+            if ('cli' !== php_sapi_name()) {
+                ExceptionHandler::register();
+            }
+        } else {
+            ini_set('display_errors', 0);
+        }
     }
 
     /**
